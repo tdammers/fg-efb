e596adc955e0a24dc1fa90801a645ed27fdfe4ad
Tobias Dammers <tdammers@gmail.com>
2023-04-07 10:41:49 +0200

    Resize guide images to smaller size

--- docs/compile.sh
+++ docs/compile.sh
@@ -7,15 +7,15 @@ for source_img in $source_images; do
     case "$(basename "$dest_img")" in
 
         paperwork-map.jpg)
-            convert "$source_img" -crop 1080x720+50+190 -quality 95 "$dest_img"
+            convert "$source_img" -crop 1080x720+50+190 -scale '50%x50%' -density 72x72 -quality 95 "$dest_img"
             ;;
 
         home-screen-annotated.jpg)
-            convert "$source_img" -quality 95 "$dest_img"
+            convert "$source_img" -scale '50%x50%' -density 72x72 -quality 95 "$dest_img"
             ;;
 
         *)
-            convert "$source_img" -crop 720x1010+150+32 -quality 95 "$dest_img"
+            convert "$source_img" -crop 720x1010+150+32 -scale '50%x50%' -density 72x72 -quality 95 "$dest_img"
             ;;
     esac
 done
